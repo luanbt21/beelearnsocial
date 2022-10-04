2ba430c414de564ec37e9911de3ebb8542bec694
Bui Thanh Luan <btluan2@cmcglobal.vn>
2022-10-04 13:26:36 +0700

    Update prisma schema

---
+++ prisma/seed.js
@@ -0,0 +1,86 @@
+import { PrismaClient } from '@prisma/client'
+
+async function main() {
+	const prisma = new PrismaClient()
+	const user = await prisma.user.create({
+		data: {
+			displayName: 'Luan',
+			email: 'luan.vy.yb@gmail.com',
+			isAnonymous: false,
+			photoURL:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGfWfmEO79zsR9zg_Qeu1DR36KDElNr3jo-w&usqp=CAU',
+			uid: '12345',
+		},
+	})
+	console.log({ user })
+
+	const tagsData = ['onthidaihoc', 'english']
+	const tagsId = []
+	for (const t of tagsData) {
+		tagsId.push(
+			await prisma.tag.upsert({
+				where: {
+					name: t,
+				},
+				update: {},
+				create: {
+					name: t,
+					description: '',
+				},
+				select: {
+					id: true,
+				},
+			}),
+		)
+	}
+	console.log({ tags: tagsId })
+
+	const post = await prisma.post.create({
+		data: {
+			authorId: user.id,
+			title: 'Post title',
+			caption: 'post caption',
+			content: 'lorem ipsum bla bla bla',
+			description: 'description',
+			tags: {
+				connect: tagsId,
+			},
+		},
+		select: {
+			id: true,
+			tagIDs: true,
+		},
+	})
+
+	console.log({ post })
+
+	const reaction = await prisma.reaction.create({
+		data: {
+			userId: user.id,
+			postId: post.id,
+			type: 'LOVE',
+			icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGfWfmEO79zsR9zg_Qeu1DR36KDElNr3jo-w&usqp=CAU',
+		},
+	})
+
+	post.tagIDs.forEach((tagId) => {
+		prisma.analytics.upsert({
+			where: {
+				reactionId: reaction.id,
+				tagId,
+				date: new Date().toDateString(),
+			},
+			update: {
+				count: { increment: 1 },
+			},
+			create: {
+				reactionId: reaction.id,
+				tagId,
+				date: new Date().toDateString(),
+				count: 1,
+			},
+		})
+	})
+}
+
+main()

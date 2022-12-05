ac63bc7c1da6490fb10c7c55d07b4dadaa69a42f
tranthihaiyen102 <11195920@st.neu.edu.vn>
2022-12-05 21:01:02 +0700

    Use adapter auto.

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapterAuto from '@sveltejs/adapter-auto'
-import adapterNode from '@sveltejs/adapter-node'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +14,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: process.env.NODE_SERVER ? adapterNode() : adapterAuto(),
+		adapter: adapterAuto(),
 		csrf: {
 			checkOrigin: false,
 		},
